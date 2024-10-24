d7a15b64495f513145801472bb17246f54849334
vspieri <viniciuspieri1632@gmail.com>
2024-10-24 13:52:38 -0300

    SD

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<Contexto>
-    (options => options.UseSqlServer("Data Source=SB-1490631\\SQLSENAI;Initial Catalog = SunTech;Integrated Security = True;TrustServerCertificate = True"));
+    (options => options.UseSqlServer("Data Source=SB-1490653\\SQLSENAI;Initial Catalog = SunTech;Integrated Security = True;TrustServerCertificate = True"));
 
 
 var app = builder.Build();
